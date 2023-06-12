c584c03224726751f1f49b7b3934e3adaa28c999
Francis009zhao <50411300+Francis009zhao@users.noreply.github.com>
2023-06-12 22:14:05 +0800

    fast json

---
+++ works/FastJsonDemo.java
@@ -0,0 +1,17 @@
+package com.zzml.flinklearn.works;
+
+/**
+ * @CopyRigth: com.zzml.flink
+ * @ClassName:FastJsonDemo
+ * @Auther: zzml
+ * @Description:
+ * @Date: 2023/6/12 22:12
+ * @Version: v1.0
+ * @ModifyDate:
+ */
+
+public class FastJsonDemo {
+
+
+
+}
