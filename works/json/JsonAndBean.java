1b5b806854ef6f6916ed39f9dae10e5d76ca39aa
Francis009zhao <50411300+Francis009zhao@users.noreply.github.com>
2023-06-17 18:23:52 +0800

    json

--- works/json/JsonAndBean.java
+++ works/json/JsonAndBean.java
@@ -33,8 +33,13 @@ public class JsonAndBean {
      * Bean 转成 json
      */
     public static void beanToJson(StudentBean studentBean){
+        // 方法一: bean转成json的方法使用的toJSON方法，将bean对象传入即可
         JSONObject jsonStu = (JSONObject) JSONObject.toJSON(studentBean);
         System.out.println(jsonStu.toJSONString());
+
+        // 方法二: 使用toJSONString() 来将bean对象传入，即可将bean对象转换为json
+        String jsonStr = JSON.toJSONString(studentBean);
+        System.out.println(">>>" + jsonStr);
 
     }
 
@@ -43,9 +48,24 @@ public class JsonAndBean {
      */
     public static void jsonToBean(String str){
 
+        // 使用parseObject方法，将json格式的数据传入，并指定bean结构的class
         StudentBean studentBean = JSON.parseObject(str, StudentBean.class);
 
+        // JSONObject 继承 JSON，因此也同样可以调用 parseObject 方法来将json转换成bean
+        StudentBean stuJson = JSONObject.parseObject(str, StudentBean.class);
+
         System.out.println(studentBean);
+
+    }
+
+    /**
+     * json格式的String类型字符串转成JsonObject
+     */
+    public static void jsonStringToJsonObject(String str){
+
+        // JSONObject调用parseObject，将json格式的String类型数据传入，即可转换成JSONObject
+        JSONObject jsonObject = JSONObject.parseObject(str);
+        System.out.println(jsonObject);
 
     }
 
